1a7e2c4f69a051ba4e9ac86d044d26bbc40c288f
Jose Luis Condori Jara <jcondori@vallegrande.edu.pe>
2019-01-04 11:25:39 -0500

    Remove All External Modules
    Hubieron errores pero el modulo funciona normal
    Se probo con una nueva construccion reinicia el sistema por cada modulo instalado

--- library/models/library_category.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class LibraryCategory(models.Model):
-    _name = 'library.category'
-
-    name = fields.Char(string="Nombre")
-    active = fields.Boolean("Esta Activo")
