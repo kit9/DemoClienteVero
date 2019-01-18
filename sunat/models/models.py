73206640a66f9f4945a0d70f2c2c099d1c9a4f01
mateochau <41794447+mateochau@users.noreply.github.com>
2019-01-18 12:09:28 -0500

    Update models.py

--- sunat/models/models.py
+++ sunat/models/models.py
@@ -145,7 +145,7 @@ class account_invoice(models.Model):
     @api.multi
     def _get_month_invoice(self):
         for rec in self:
-            if rec.date_invoice != True:
+            if rec.date_invoice != False:
                 rec.month_year_inv = rec.date_invoice.strftime("%m%Y")
 
     def _generate_txt_bill(self):
