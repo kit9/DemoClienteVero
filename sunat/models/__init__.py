35d1cd26854e155c6bfcbe0e621df6f4f17bf402
Jose Luis Condori Jara <jcondori@vallegrande.edu.pe>
2019-02-21 21:06:32 -0500

    Limpiar

--- sunat/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models
-from . import account_invoice
-from . import purchase_order
-# from . import account_payment
-from . import stock_move_line
-from . import exchange_rate
-from . import sale_order
