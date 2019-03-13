e1c067e1bcf6723bb53a9202dfaae3f64c8eb5a6
Jordy Valenzuela <jvalenzuela@vallegrande.edu.pe>
2019-03-13 16:17:22 -0500

    prueba de error

--- libreria/VG/__manifest__.py
+++ libreria/VG/__manifest__.py
@@ -30,7 +30,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'wizard/chart_of_accounts_view.xml',
+        'wizard/chart_accounts_view.xml',
         #'wizard/record_of_actives_view.xml',
         'wizard/four_retentions_view.xml',
         'wizard/not_domiciled_view.xml',
