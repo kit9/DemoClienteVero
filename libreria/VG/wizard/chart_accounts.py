6a59ef2bd568809ce499ae82a21510bc2ceff1f2
Jordy Valenzuela <jvalenzuela@vallegrande.edu.pe>
2019-03-13 16:18:06 -0500

    prueba error

--- libreria/VG/wizard/chart_accounts.py
+++ libreria/VG/wizard/chart_accounts.py
@@ -7,7 +7,7 @@ _logger = logging.getLogger(__name__)
 
 
 class chartofaccounts(models.TransientModel):
-    _name = "libreria.chart_of_accounts"
+    _name = "libreria.chart_accounts"
     _description = "Plan Contable"
 
     state = fields.Selection(
@@ -61,7 +61,7 @@ class chartofaccounts(models.TransientModel):
         return {
             'type': 'ir.actions.act_window',
             'name': 'Plan Contable',
-            'res_model': 'libreria.chart_of_accounts',
+            'res_model': 'libreria.chart_accounts',
             'view_mode': 'form',
             'view_type': 'form',
             'res_id': self.id,
