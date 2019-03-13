f89229d09324484136958a3b95626ac1d4d5fe9a
Jordy Valenzuela <jvalenzuela@vallegrande.edu.pe>
2019-03-13 11:10:45 -0500

    retenciones 4th (1° prueba)

--- libreria/VG/wizard/not_domiciled.py
+++ libreria/VG/wizard/not_domiciled.py
@@ -21,52 +21,65 @@ class not_domiciled(models.TransientModel):
 
         lst_account_move_line = self.env['account.invoice'].search([])
         content_txt = ""
+        estado_ope = ""
         # Iterador - Jcondori
         for line in lst_account_move_line:
-            # Asiento Conta
+            # Asiento Contable
+            if line.create_date.strftime("%m%Y") == time.strftime("%m%Y"):
+                estado_ope = "1"
+             else:
+                 if line.create_date.strftime("%Y") != time.strftime("%Y"):
+                     estado_ope = "0"
+                 else:
+                     estado_ope = "0"
 
-            # por cada campo encontrado daran una linea como mostrare
-            txt_line = "%s|%s|%s|%s|%s|%s|%s|%s|%s" \
-                       "|%s|%s|%s|%s|%s|%s|%s|%s|%s" \
-                       "|%s|%s|%s|%s|%s|%s|%s|%s|%s" \
-                       "|%s|%s|%s|%s|%s|%s|%s|%s|%s" % (
+            # por cada campo encontrado daran una linea como mostrare , Hay 11,10,10,10
+            txt_line = "%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s" \
+                       "|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s" \  
+                       "|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s" \
+                       "|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s" % (
                            # Facturas de proveedor
-                           line.date_invoice or '',  # 1 Hoja 1 (Fecha Contable)
-                           line.move_id.x_studio_field_fwlP9 or '',  # 2 Hoja 2 (Asiento Contable/ID)
-                           line.move_id or '',  # 3 Hoja 3 (Asiento Contable)
-                           line.date_document or '',  # 4 Hoja 4 (Fecha de Documento)
-                           line.document_type_id or '',  # 5 Hoja 5 (Tipo de Documento)
-                           line.invoice_serie or '',  # 6 Hoja 6 (Serie)
-                           line.invoice_number or '',  # 7 Hoja 7 (Numero)
-                           line.base_imp or '',  # 8 Hoja 8 (Base Imponible)
-                           '',  # 9 Hoja 8 (Tipo de Cambio
-                           line.amount_total or '',  # 10 Hoja 10 (Total)
-                           line.exchange_rate or '',  # 11 Hoja 10 (Tipo de Cambio)
-                           '',  # 12 Hoja 11 (Tipo de persona: natural-juridica)
-                           line.invoice_number or '',  # 13 Hoja 12 (Numero)
-                           line.year_emission_dua or '',  # 14 Hoja 13 (Año de la Emision de la DUA)
-                           line.invoice_number or '',  # 15 Hoja 14 (Numero)
-                           line.exchange_rate or'',  # 16 Hoja 16 (Tipo de Cambio)
-                           '',  # 17 Hoja 17 null
-                           '',  # 18 Hoja 18
-                           '',  # 19 Hoja 19
-                           '',  # 20 Hoja 20
-                           '',  # 21 Hoja 21 (RUC objeto / campo, einvoice.catalog.06.catalog_06_id)
-                           '',  # 22 Hoja 22 null
-                           '',  # 23 Hoja 23
-                           '',  # 24 Hoja 24
-                           '',  # 25 Hoja 25 null
-                           '',  # 26 Hoja 26 null
-                           '',  # 27 Hoja 27 null
-                           '',  # 28 Hoja 28 null
-                           '',  # 29 Hoja 29 null
-                           '',  # 30 Hoja 30 null
-                           '',  # 31 Hoja 31
-                           line.x_studio_exoneraciones or '',  # 32 Hoja 32 (Exoneraciones)
-                           '',  # 33 Hoja 33 (Hay dos tipos de renta: x_studio_tipo_de_renta, type_income_id
-                           line.x_studio_modalidad_de_servicio or '',  # 34 Hoja 34 (Modalidad de Servicio)
-                           line.message_needaction or '',  # 35 Hoja 35 (Aplicacion parrafo art. 76)
-                           ''  # 36 Hoja
+                           line.date_invoice or '',                     # 01 Hoja 1 (Fecha Contable)
+                           line.move_id.x_studio_field_fwlP9 or '',     # 02 Hoja 2 (Asiento Contable/ID)
+                           line.move_id or '',                          # 03 Hoja 3 (Asiento Contable)
+                           line.date_document or '',                    # 04 Hoja 4 (Fecha de Documento)
+                           line.document_type_id or '',                 # 05 Hoja 5 (Tipo de Documento)
+                           line.invoice_serie or '',                    # 06 Hoja 6 (Serie)
+                           line.invoice_number or '',                   # 07 Hoja 7 (Numero)
+                           line.amount_untaxed or '',                   # 08 Hoja 8 (Base Imponible)
+                           line.exchange_rate or '',                    # 09 Hoja 8 (Tipo de Cambio)
+                           line.tax_line_ids or '',                     # 10 Hoja 9 (Impuestos)
+                           line.amount_untaxed or '',                   # 11 Hoja 9 (Base Imponible)
+                           line.exchange_rate or '',                    # 12 Hoja 9 (Tipo de Cambio)
+                           line.amount_total or '',                     # 13 Hoja 10 (Total)
+                           line.exchange_rate or '',                    # 14  Hoja 10 (Tipo de Cambio)
+                           line.partner_id.person_type or'',            # 15 Hoja 11 (Tipo de persona: natural-juridica)
+                           line.invoice_number or '',                   # 16 Hoja 12 (Numero)
+                           line.year_emission_dua or '',                # 17 Hoja 13 (Año de la Emision de la DUA)
+                           line.invoice_number or '',                   # 18 Hoja 14 (Numero)
+                           line.amount or '',                           # 19 Hoja 15 (Cantidad a Pagar)
+                           line.state or '',                            # 20 Hoja 15 (Estado)
+                           line.exchange_rate or '',                    # 21 Hoja 16 (Tipo de Cambio)
+                           '',                                          # 22 Hoja 17 null
+                           line.partner_id.country_id or'',             # 23 Hoja 18
+                           line.partner_id or '',                       # 24 Hoja 19 (Proveedor)
+                           line.partner_id.street or '',                # 25 Hoja 20 (Address, Direccion)
+                           line.partner_id.catalog_06_id or'',          # 26 Hoja 21 (RUC)
+                           '',                                          # 27 Hoja 22 null
+                           line.partner_id.name or '',                  # 28 Hoja 23 (Nombre de Contacto)
+                           line.partner_id.title or '',                 # 29 Hoja 24 (El contacto es : "socio")
+                           '',                                          # 30 Hoja 25 null
+                           '',                                          # 31 Hoja 26 null
+                           '',                                          # 32 Hoja 27 null
+                           '',                                          # 33 Hoja 28 null
+                           '',                                          # 34 Hoja 29 null
+                           '',                                          # 35 Hoja 30 null
+                           line.partner_id.x_studio_convenios or '',    # 36 Hoja 31 (Convenios)
+                           line.x_studio_exoneraciones or '',           # 37 Hoja 32 (Exoneraciones)
+                           line.type_income_id or '',                   # 38 Hoja 33 (Hay dos tipos de renta: x_studio_tipo_de_renta, type_income_id)
+                           line.x_studio_modalidad_de_servicio or '',   # 39 Hoja 34 (Modalidad de Servicio)
+                           line.message_needaction or '',               # 40 Hoja 35 (Aplicacion parrafo art. 76)
+                           ''                                           # 41 Hoja 36
 
             )
 
