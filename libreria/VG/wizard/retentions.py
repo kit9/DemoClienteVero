d8048d74bf262954810f1b2c4fb947dfeb5db0b4
Jordy Valenzuela <jvalenzuela@vallegrande.edu.pe>
2019-03-15 12:21:30 -0500

    prueba6(Anthony)

--- libreria/VG/wizard/retentions.py
+++ libreria/VG/wizard/retentions.py
@@ -61,9 +61,8 @@ class chartofaccounts(models.TransientModel):
                     else:
                         _estado_ope = "09"
 
-            # por cada campo encontrado daran una linea como mostrare
-            txt_line = "%s|%s|%s|%s|M%s|%s|%s|%s|%s|%s|%s|%s" \
-                    (
+                # por cada campo encontrado daran una linea como mostrare
+            txt_line = "%s|%s|%s|%s|M%s|%s|%s|%s|%s|%s|%s|%s" % (
                     line.payment_date.strftime("%Y%m00") or '',  # 1
                     line.journal_id or '',  # 1
                     line.state or '',  # 1 null
