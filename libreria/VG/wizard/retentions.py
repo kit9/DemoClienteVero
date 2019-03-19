15a6441ccd535225513aa17fcf21b7e20fc1fa88
Anthony Loayza <31226781+tonimontana1697@users.noreply.github.com>
2019-03-19 20:06:20 +0100

    Update retentions.py

--- libreria/VG/wizard/retentions.py
+++ libreria/VG/wizard/retentions.py
@@ -47,7 +47,7 @@ class chartofaccounts(models.TransientModel):
 
             # factura
             for imp3 in line.invoice_ids:
-                if imp3.amount_total * imp3.exchange_rate
+                if imp3.amount_total * imp3.exchange_rate:
                    _factura = imp3.amount_total * imp3.exchange_rate
 
             if line.create_date.strftime("%m%Y") == time.strftime("%m%Y"):
