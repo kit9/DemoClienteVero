1dee095ee0fa4b1e9e65cfb202fb50fbcb15f633
Anthony Loayza <31226781+tonimontana1697@users.noreply.github.com>
2019-03-21 22:07:06 -0500

    Update retentions.py

--- libreria/VG/wizard/retentions.py
+++ libreria/VG/wizard/retentions.py
@@ -19,7 +19,7 @@ class chartofaccounts(models.TransientModel):
     def generate_file(self):
         # Data - Jcondori
 
-        lst_account_move_line = self.env['account.move'].search(['&',('id','=','30')])
+        lst_account_move_line = self.env['account.move'].search([('id','=','30')])
         content_txt = ""
         _factura = ""
         _numero = ""
