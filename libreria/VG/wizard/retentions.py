f6bcf89c7263328de6cfdcb1155c8c771d60f295
Anthony Loayza <31226781+tonimontana1697@users.noreply.github.com>
2019-03-26 22:28:50 -0500

    Update retentions.py

--- libreria/VG/wizard/retentions.py
+++ libreria/VG/wizard/retentions.py
@@ -21,7 +21,7 @@ class retentions(models.TransientModel):
     @api.multi
     def generate_file(self):
         #filtro fecha
-        dominio1 = [('month_year_move', 'like', self.date_month + "" + self.date_year),('journal_id.name','like','Retenciones')]
+        dominio1 = [('month_year_move', 'like', self.date_month + "" + self.date_year),('%','journal_id.name','like','Retenciones')]
 
         lst_account_move_line = self.env['account.move'].search([(dominio1)])
         content_txt = ""
