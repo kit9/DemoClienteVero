888d1d10a94910bc54b9bf294ece9a23786c0b39
Cesar Barraza <cbarraza@vallegrande.edu.pe>
2019-04-01 10:43:45 -0500

    Update record_actives.py

--- libreria/VG/wizard/record_actives.py
+++ libreria/VG/wizard/record_actives.py
@@ -42,14 +42,14 @@ class record_actives(models.TransientModel):
                 if line.category_id.account_asset_id.company_id.id:
                     v1 = line.category_id.account_asset_id.company_id.id
             # por cada campo encontrado daran una linea como mostrare
-            txt_line = "%s|%s|%s|%s|%s" % (
+            txt_line = "%s|%s|%s|%s|%s|%s|%s" % (
                            line.date.strftime("%Y%m00") or '',  # 1 jvalenzuela
                            line.invoice_id.move_id.name or '',  # 2 jvalenzuela
                            '',  # 3 jvalenzuela (no se encuentra)
                            '',  # 4 jvalenzuela (no se encuentra)
                            line.name or '',  # 5 rloayza
-                           # # '',  # 6 rloayza (no se encontro)
-                           # # line.name or '',  # 7 rloayza
+                           '',  # 6 rloayza (no se encontro)
+                           line.name or '',  # 7 rloayza
                            # # v1[0:6] or '',  # 8 rloayza
                            # # line.entry_count or '',  # 9 rloayza
                            # # line.category_id or '',  # 10 rloayza
