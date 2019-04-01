920738009db910cd52d96e2b828a2003b0cac2f5
anthony loayza <rloayza@vallegrande.edu.pe>
2019-04-01 12:50:51 -0500

    Update retentions.py

--- libreria/VG/wizard/retentions.py
+++ libreria/VG/wizard/retentions.py
@@ -22,7 +22,7 @@ class retentions(models.TransientModel):
     def generate_file(self):
         # filtro fecha
         # dominio1 = [('month_year_move', 'like', self.date_month + "" + self.date_year),('journal_id.name', 'like', 'Retenciones')]
-        dominio2 = fields ('journal_id.name', string='Retenciones')
+        dominio2 = fields ('account.journal.name', string='Retenciones')
 
         lst_account_move_line = self.env['account.move'].search([dominio2])
         content_txt = ""
