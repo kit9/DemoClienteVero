7f958eac89b04b1f98b7b3f3883d4fc4f5ceb9f9
Cesar Barraza <cbarraza@vallegrande.edu.pe>
2019-04-01 09:30:51 -0500

    update

--- libreria/VG/wizard/record_actives.py
+++ libreria/VG/wizard/record_actives.py
@@ -48,8 +48,8 @@ class record_actives(models.TransientModel):
                        "|%s|%s|%s|%s|%s|%s|%s|%s|%s" \
                        "|%s|%s|%s|%s|%s|%s|%s|%s|%s" \
                        "|%s|%s|%s|%s|%s|%s|%s|%s|%s" % (
-                           line.date.strftime("%Y%m00") or '',  # 1 jvalenzuela
-                           line.code or '',  # 2 jvalenzuela
+                           line.date or '',  # 1 jvalenzuela
+                           line.invoice_id.move_id.name or '',  # 2 jvalenzuela
                            '',  # 3 jvalenzuela (no se encuentra)
                            '',  # 4 jvalenzuela (no se encuentra)
                            line.name or '',  # 5 rloayza
