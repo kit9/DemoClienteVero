6ef61223d1bb1247a3f69929ea5fcd2c5ccd15f9
Jose Luis Condori Jara <jcondori@vallegrande.edu.pe>
2019-04-01 22:15:37 -0500

    Update retentions.py

--- libreria/VG/wizard/retentions.py
+++ libreria/VG/wizard/retentions.py
@@ -24,7 +24,7 @@ class retentions(models.TransientModel):
         dominio1 = [('month_year_move', 'like', self.date_month + "" + self.date_year),('journal_id.name', 'like', 'Retenciones')]
         # dominio2 = fields.Many2one ('account.journal.name', string='Retenciones')
 
-        lst_account_move_line = self.env['account.move'].search([dominio1])
+        lst_account_move_line = self.env['account.move'].search(dominio1)
         content_txt = ""
         _factura = ""
         _numero = ""
