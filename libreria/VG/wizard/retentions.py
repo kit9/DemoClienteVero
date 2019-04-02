bab5bbfd02053ddb7dd98022ca84af83606219e3
Anthony Loayza <rloayza@vallegrande.edu.pe>
2019-04-02 17:20:30 -0500

    Update retentions.py

--- libreria/VG/wizard/retentions.py
+++ libreria/VG/wizard/retentions.py
@@ -40,7 +40,8 @@ class retentions(models.TransientModel):
             if imp in line.line_ids:
                      if imp.invoice_id:
                         if imp.invoice_id.document_type_id:
-                           _factura = imp.invoice_id.document_type_id.number
+                           _factura = imp.invoice_id.document_type_id
+
 
              #for imp in line.journal_id.company_partner_id.unreconciled_aml_ids:
              #          if imp.invoice_id.document_type_id:
