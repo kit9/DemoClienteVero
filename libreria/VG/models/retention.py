38f129922d2dc865c9eb0b99fd0591f3e2fc00af
Jordy Valenzuela <jvalenzuela@vallegrande.edu.pe>
2019-04-03 12:40:27 -0500

    retenciones

---
+++ libreria/VG/models/retention.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class retentions(models.Model):
+    _inherit = 'account.move'
+
+    # Para filtrar
+    month_year_move = fields.Char(compute="_get_month_move", store=True, copy=False)
+
+    @api.multi
+    @api.depends('create_date')
+    def _get_month_move(self):
+        for rec in self:
+            if rec.create_date:
+                rec.month_year_move = rec.create_date.strftime("%m%Y")
