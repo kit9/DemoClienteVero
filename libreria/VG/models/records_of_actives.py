511c781e6ad85b66ed5582c4fc10c4f2f888f908
Jordy Valenzuela <jvalenzuela@vallegrande.edu.pe>
2019-04-04 10:51:35 -0500

    p2

--- libreria/VG/models/records_of_actives.py
+++ libreria/VG/models/records_of_actives.py
@@ -3,12 +3,11 @@
 from odoo import models, fields, api
 import logging
 
-
 _logger = logging.getLogger(__name__)
 
 
-class RecordOfActives(models.Model):
+class ChartAccount(models.Model):
     _inherit = 'account.asset.asset'
 
-    Cod_Catalog = fields.Char(string="Codigo de catalogo")
-    Cod_Existent = fields.Char(string="Codigo de Existencia")
+    Cod_Catalog = fields.Char(compute="_compute_cod_catalog", store=True, copy=False)
+    Cod_Existent = fields.Char(compute="_compute_cod_existent", store=True, copy=False)
