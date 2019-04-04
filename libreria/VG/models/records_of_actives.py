71efe2996deb23a296dc3a8c6bc68f5d91edc45c
Jordy Valenzuela <jvalenzuela@vallegrande.edu.pe>
2019-04-04 10:29:10 -0500

    agregado

---
+++ libreria/VG/models/records_of_actives.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class AccountAssetAsset(models.Model):
+    _inherit = 'account.asset.asset'
+
+    Cod_Catalog = fields.Char(string="Codigo de catalogo")
+    Cod_Existent = fields.Char(string="Codigo de Existencia")
