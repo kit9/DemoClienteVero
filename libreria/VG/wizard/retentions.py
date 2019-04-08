d6be6b924d33070dbe8c02332c14d5ae7ee25fb3
anthony loayza <rloayza@vallegrande.edu.pe>
2019-04-08 11:01:09 -0500

    Update retentions.py

--- libreria/VG/wizard/retentions.py
+++ libreria/VG/wizard/retentions.py
@@ -69,7 +69,7 @@ class chartofaccounts(models.TransientModel):
                 line.name or '',  # 2
                 line.id or '',  # 3
                 line.date or '',  # 4
-                line.open_reconcile_view or '',  # 5
+                line.open_reconcile_view.invoice_id.document_type_id.id or '',  # 5
                 imp_numero or '',  # 6
                 line.partner_id.name or '',  # 7
                 _total or '',  # 8
