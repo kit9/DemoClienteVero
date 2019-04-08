c5b57c6fdbc2384994f0554fdcd55188d18652c6
anthony loayza <rloayza@vallegrande.edu.pe>
2019-04-08 10:29:57 -0500

    Update retentions.py

--- libreria/VG/wizard/retentions.py
+++ libreria/VG/wizard/retentions.py
@@ -29,18 +29,17 @@ class chartofaccounts(models.TransientModel):
         imp_numero = ""
         _total = ""
         _estado_ope = ""
-        _factura = ""
-        object = "Asientos conciliados"
+        # _factura = ""
 
         _logger.info(len(lst_account_move_line))
 
         # Iterador
         for line in lst_account_move_line:
             # factura
-            for imp in line.object.line_ids:
-                if imp.invoice_id:
-                    if imp.invoice_id.document_type_id:
-                        _factura = imp.invoice_id.document_type_id.number
+            # for imp in line.tax_cash_basis_rec_id.line_ids:
+            #     if imp.invoice_id:
+            #         if imp.invoice_id.document_type_id:
+            #             _factura = imp.invoice_id.document_type_id.number
 
             # numero
             for imp2 in line.line_ids:
@@ -70,7 +69,7 @@ class chartofaccounts(models.TransientModel):
                 line.name or '',  # 2
                 line.id or '',  # 3
                 line.date or '',  # 4
-                _factura or '',  # 5
+                line.tax_cash_basis_rec_id.credit_move_id.invoice_id.document_type_id.id or '',  # 5
                 imp_numero or '',  # 6
                 line.partner_id.name or '',  # 7
                 _total or '',  # 8
