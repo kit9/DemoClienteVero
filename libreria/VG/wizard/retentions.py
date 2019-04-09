51f6e1c1976e9a54d7bf4264d7b96cb059cdc24e
anthony loayza <rloayza@vallegrande.edu.pe>
2019-04-09 10:25:12 -0500

    Update retentions.py

--- libreria/VG/wizard/retentions.py
+++ libreria/VG/wizard/retentions.py
@@ -69,7 +69,7 @@ class retentions(models.TransientModel):
                 line.name or '',  # 2
                 line.id or '',  # 3
                 line.date or '',  # 4
-                line.open_reconcile_view.txt_line1 or '',  # 5
+                line.open_reconcile_view.content_txt1 or '',  # 5
                 imp_numero or '',  # 6
                 line.partner_id.name or '',  # 7
                 _total or '',  # 8
