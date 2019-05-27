841ddac7a546ededfe5b9471d37a1436278f6bb4
Cesar Barraza <cbarraza@vallegrande.edu.pe>
2019-05-27 11:30:24 -0500

    Update account_12_13.py

--- libreria/VG/wizard/account_12_13.py
+++ libreria/VG/wizard/account_12_13.py
@@ -48,7 +48,7 @@ class Account_12_13(models.TransientModel):
                 line.partner_id.vat or '', # Tipo de Doc. Identidad - RUC, enteros
                 #line.partner.registration_name or '', # NADRS
                 line.partner_id.registration_name or '',
-                line.date_document or '',
+                line.account_invoice.date_document or '',
                 '',
                 '',
                 ''
