2340430fab39cce5490c2a2d66e660c89dacecdc
jordy valenzuela <jvalenzuela@vallegrande.edu.pe>
2019-05-28 13:06:35 -0500

    Update account_10.py

--- libreria/VG/wizard/account_10.py
+++ libreria/VG/wizard/account_10.py
@@ -21,7 +21,7 @@ class account_10(models.TransientModel):
     def generate_file(self):
 
         # modelo a buscar
-        lst_account_move_line = self.env['account.payment'].search([])
+        lst_account_move_line = self.env['account.bank_account_id'].search([])
 
         # variables creadas
         diario = ""
