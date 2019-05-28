27e4db7a3901d312dd3f7a92578e1aadf69c68df
Jordy Valenzuela <jvalenzuela@vallegrande.edu.pe>
2019-05-27 22:35:31 -0500

    prueba 6

--- libreria/VG/wizard/account_detail.py
+++ libreria/VG/wizard/account_detail.py
@@ -63,7 +63,7 @@ class ChartAccount(models.TransientModel):
                 line.invoice_id.invoice_number or '',  #
                 line.invoice_id.date_document or '',  #
                 debe or '',  #
-                line.display_name or ''   # estado de operacion
+                line.name or ''   # estado de operacion
 
             )
 
