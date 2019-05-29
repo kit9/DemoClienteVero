8a9a96916637ee26541097a24f89fb8222f9d68f
Cesar Barraza <cbarraza@vallegrande.edu.pe>
2019-05-29 12:13:14 -0500

    Update account_17.py

--- libreria/VG/wizard/account_17.py
+++ libreria/VG/wizard/account_17.py
@@ -51,10 +51,11 @@ class Account_17(models.TransientModel):
                    _nombre = imp3.partner_id.name
 
             # validador de estado de operación
-            if line.date_maturity.strftime("%m%Y") == time.strftime("%m%Y"):
+            for p3 in line.line_ids:
+            if line.line_ids.date_maturity.strftime("%m%Y") == time.strftime("%m%Y"):
                 estado_ope = "01"
             else:
-                if line.date_maturity.strftime("%Y") != time.strftime("%Y"):
+                if line.line_ids.date_maturity.strftime("%Y") != time.strftime("%Y"):
                     estado_ope = "09"
                 else:
                     if int(time.strftime("%m")) == int(time.strftime("%m")) - 1:
