3325f805045f366b738e92c6e83f9374066021ba
jrejasyman <jrejas@vallegrande.edu.pe>
2019-05-29 10:57:42 -0500

    Update account_10.py

--- libreria/VG/wizard/account_10.py
+++ libreria/VG/wizard/account_10.py
@@ -32,7 +32,7 @@ class account_10(models.TransientModel):
                 line.payment_date.strftime("%Y%m00") or '',
                 '',
                 line.journal_id.code or'',
-                line.bank_account_id.acc_number or '',
+                line.acc_number or '',
                 '',
                 '',
                 ''
