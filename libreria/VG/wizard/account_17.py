f8ba81e9bf5ddeb95b9f842e9991a74f87cebc78
Cesar Barraza <cbarraza@vallegrande.edu.pe>
2019-05-29 10:49:30 -0500

    Update account_17.py

--- libreria/VG/wizard/account_17.py
+++ libreria/VG/wizard/account_17.py
@@ -70,8 +70,8 @@ class Account_17(models.TransientModel):
                 line.partner_id.name or '',
                 line.partner_id.vat or '',
                 line.partner_id.name or '',
-                line.date.strftime("") or '',
-                '',
+                line.date.strftime("%d/%m/%Y") or '',
+                line.debit or '',
                 '',
                 '',
             )
