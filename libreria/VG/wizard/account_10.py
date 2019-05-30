fd93a054b28ce093300644995a5446685b0d443f
jrejasyman <jrejas@vallegrande.edu.pe>
2019-05-30 11:24:55 -0500

    Update account_10.py

--- libreria/VG/wizard/account_10.py
+++ libreria/VG/wizard/account_10.py
@@ -19,9 +19,10 @@ class account_10(models.TransientModel):
 
         # modelo a buscar
         lst_account_move_line = self.env['account.payment'].search([])
+
         # variables creadas
         content_txt = ""
-        
+
         # Iterador
         for line in lst_account_move_line:
 
