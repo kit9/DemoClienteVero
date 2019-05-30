1aaa984eee760905020a1ac2a9a9da60f45a3318
Luis De la Cruz <ldelacruz@vallegrande.edu.pe>
2019-05-29 19:24:54 -0500

    Update account_12_13.py

--- libreria/VG/wizard/account_12_13.py
+++ libreria/VG/wizard/account_12_13.py
@@ -17,7 +17,7 @@ class Account_12_13(models.TransientModel):
     @api.multi
     def generate_file(self):
         # modelo a buscar
-        lst_account_move_line = self.env['account.move'].search([('12','line_ids.account_id.code', 'ilike')])
+        lst_account_move_line = self.env['account.move'].search([('line_ids.account_id.code', 'ilike','12')])
 
         # variables creadas
         content_txt = ""
@@ -31,7 +31,7 @@ class Account_12_13(models.TransientModel):
 
             # referencia
             #for imp in line.line_ids:
-               ## for imp1 in imp.invoice_id:
+                #for imp1 in imp.invoice_id:
 
 
             # Catalogo
