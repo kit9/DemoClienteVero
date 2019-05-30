f7b8d77b109f5c7df5266436752589509407cf2f
Luis De la Cruz <ldelacruz@vallegrande.edu.pe>
2019-05-29 19:27:18 -0500

    Update account_12_13.py

--- libreria/VG/wizard/account_12_13.py
+++ libreria/VG/wizard/account_12_13.py
@@ -59,8 +59,8 @@ class Account_12_13(models.TransientModel):
 
             txt_line = "%s|%s|%s|%s|%s|%s|%s|%s|%s" % (
                 line.date.strftime("%Y%m00") or '', #1 Periodo- Fecha contable
-         -       line.ref or '',  # 2 ASIENTO CONTABLE
-         -       line.x_studio_field_fwlP9 or '',  # 3 Asiento contable _ ID
+                line.ref or '',  # 2 ASIENTO CONTABLE
+                line.x_studio_field_fwlP9 or '',  # 3 Asiento contable _ ID
                 _catalogo or '', #4 ID - RUC
                 line.partner_id.vat or '',  # 5 Tipo de Doc. Identidad - RUC, enteros
                 line.partner_id.registration_name or '',  # 6 Nombre de la empresa
