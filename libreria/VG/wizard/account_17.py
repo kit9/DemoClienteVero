13cb9b13f61536c8573a913b61b56245a96660db
Cesar Barraza <cbarraza@vallegrande.edu.pe>
2019-06-04 11:27:13 -0500

    Update account_17.py

--- libreria/VG/wizard/account_17.py
+++ libreria/VG/wizard/account_17.py
@@ -35,10 +35,9 @@ class Account_17(models.TransientModel):
                 if imp.debit:
                     _debito = imp.debit
 
-            # _catalogo
             for imp1 in line.line_ids:
                 if imp1.partner_id.catalog_06_id.code:
-                 _catalogo = imp1.partner_id.catalog_06_id.code
+                    _catalogo = imp1.partner_id.catalog_06_id.code
 
             # _vat
             for imp2 in line.line_ids:
@@ -70,11 +69,11 @@ class Account_17(models.TransientModel):
 
             # datos a exportar a txt
 
-            txt_line = "%s|%s|%s|%s|%s|%s|%s|%s|%s|%s" % (
+            txt_line = "%s|%s|M%s|%s|%s|%s|%s|%s|%s|%s" % (
                 line.date.strftime("%Y%m00") or '',  # 1 fecha en formato codigo
                 line.name.replace("/", "") or '', #2 nombre de la factura
                 line.x_studio_field_fwlP9 or '', #3 codigo de almacenamiento
-                line.partner_id.name or '', #4 nombre de la compañia a quien se brindo el servicio
+                _catalogo or '', #4 codigo de la compañia a quien se brindo el servicio
                 line.partner_id.vat or '', #5 ruc de la empresa
                 line.partner_id.name or '', #6 nombre de la empresa
                 line.date.strftime("%d/%m/%Y") or '', #7 fecha de elaboración
