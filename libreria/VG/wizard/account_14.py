d32c1c931c9c47948db9fce761284d023c285f2a
Anthony Loayza <rloayza@vallegrande.edu.pe>
2019-06-04 19:00:58 -0500

    Update account_14.py

--- libreria/VG/wizard/account_14.py
+++ libreria/VG/wizard/account_14.py
@@ -66,8 +66,8 @@ class Account_14(models.TransientModel):
 
             # datos a exportar a txt
 
-            txt_line = "%s|%s|M%s|%s|%s|%s|%s|%s0|%s" % (
-                line.date or '',  # 1
+            txt_line = "%s|%s|M%s|%s|%s|%s|%s|%s3f|%s" % (
+                line.date.replace("-", "") or '',  # 1
                 line.name.replace("/", "") or '',
                 line.x_studio_field_fwlP9 or '',
                 _catalogo or '',
