fd8e90afde63e3fa916eb70b22c36dcb931e37b5
Luis De la Cruz <ldelacruz@vallegrande.edu.pe>
2019-06-27 22:55:35 -0500

    Update account_12_13.py

--- libreria/VG/wizard/account_12_13.py
+++ libreria/VG/wizard/account_12_13.py
@@ -52,10 +52,10 @@ class Account_12_13(models.TransientModel):
                 #         _resid = res1.residual
 
                 #si no hay factura
-                if line.ref != _fact:
-                    _sinFact = "1"
-                elif line.ref == _fact:
+                if line.ref == _fact:
                     _sinFact = line.ref
+                elif line.ref != _fact:
+                    _sinFact = statement_id.date
 
 
             #Estado de operacion
