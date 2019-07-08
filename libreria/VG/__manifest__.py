3d84c47c225f4bf56fe0401c59a6b004e02009d3
jordy valenzuela <jvalenzuela@vallegrande.edu.pe>
2019-07-08 11:00:55 -0500

    Creacion Libro Caja y Bancos Mov. en Efectivo

--- libreria/VG/__manifest__.py
+++ libreria/VG/__manifest__.py
@@ -42,6 +42,7 @@
         'wizard/account_17_view.xml',
         'wizard/sales_view.xml',
         'wizard/account_30_view.xml',
+        'wizard/Book_Cash_and_Banks_view.xml',
         'views/menu.xml',
     ],
     # only loaded in demonstration mode
