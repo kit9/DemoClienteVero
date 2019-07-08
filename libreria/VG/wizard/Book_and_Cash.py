bf638ebbd93f4677e228ce0f77779dddb88536f8
jordy valenzuela <jvalenzuela@vallegrande.edu.pe>
2019-07-08 11:22:01 -0500

    Update Book_and_Cash.py

--- libreria/VG/wizard/Book_and_Cash.py
+++ libreria/VG/wizard/Book_and_Cash.py
@@ -5,38 +5,22 @@ import time
 
 _logger = logging.getLogger(__name__)
 
+
 ###########################################################################################
 # -- OPTIMIZA
-# -- DESCRIPCION: CLASE CHART_ACCOUNT CREACION PARA PROYECTO ODOO
+# -- DESCRIPCION: CLASE ACCOUNT_DETAIL CREACION PARA PROYECTO ODOO
 # -- AUTOR: JORDY VALENZUELA VALCARCEL
 # -- CAMBIOS: ID     FECHA (DD/MM/YYYY)  PERSONA               CAMBIOS EFECTUADOS
-# --          #001   13/03/2019          JORDY VALENZUELA          CREACION DE LA CLASE.
-# --          #002   13/03/2019          JORDY VALENZUELA          AGREGADO DE CAMPOS
-# --          #003   13/03/2019          JORDY VALENZUELA          VALIDACION DE CAMPOS
-# --          #004   13/03/2019          JORDY VALENZUELA          AGREGADO DE FILTROS.
+# --          #001   27/05/2019          JORDY VALENZUELA          CREACION DE LA CLASE.
+# --          #002   27/05/2019          JORDY VALENZUELA          AGREGADO DE CAMPOS
+# --          #003   27/05/2019          JORDY VALENZUELA          VALIDACION DE CAMPOS
+# --          #004   27/05/2019          JORDY VALENZUELA          AGREGADO DE FILTROS.
 # -----------------------------------------------------------------------------------------
 
 #   Inicio #001 "CREACION DE LA CLASE"
-class Book_and_Cash(models.TransientModel):
+class ChartAccount(models.TransientModel):
     _name = "libreria.Book_and_Cash"
     _description = "Libro Caja y Bancos"
-
-
-#   Inicio #004 "AGREGADO DE FILTROS"
-#     date_month = fields.Selection(string="Mes", selection=[('01', 'Enero'),
-#                                                            ('02', 'Febrero'),
-#                                                            ('03', 'Marzo'),
-#                                                            ('04', 'Abril'),
-#                                                            ('05', 'Mayo'),
-#                                                            ('06', 'Junio'),
-#                                                            ('07', 'Julio'),
-#                                                            ('08', 'Agosto'),
-#                                                            ('09', 'Septiembre'),
-#                                                            ('10', 'Octubre'),
-#                                                            ('11', 'Noviembre'),
-#                                                            ('12', 'Diciembre')])
-#     date_year = fields.Char(string="Año", size=4)
-#   Fin #004
 
     state = fields.Selection([('choose', 'choose'), ('get', 'get')], default='choose')
     txt_filename = fields.Char('filename', readonly=True)
@@ -45,13 +29,12 @@ class Book_and_Cash(models.TransientModel):
     @api.multi
     def generate_file(self):
 
-        # filtro de fecha
-        #dominio = [('month_year_inv', 'like', self.date_month + "" + self.date_year)]
 
-        # modelo a buscar
+
+# modelo a buscar
         lst_account_move_line = self.env['account.move'].search([])
 
-        # variables creadas
+# variables creadas
         content_txt = ""
         cuenta = ""
         moneda = ""
