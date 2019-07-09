19d49e7afb19a219bcb216ad51cf8d797050aab6
jrejasyman <jrejas@vallegrande.edu.pe>
2019-07-09 12:49:03 -0500

    Update __manifest__.py

--- libreria/VG/__manifest__.py
+++ libreria/VG/__manifest__.py
@@ -42,7 +42,7 @@
         'wizard/account_17_view.xml',
         'wizard/sales_view.xml',
         'wizard/account_30_view.xml',
-        'wizard/account_34_view.xml'
+        'wizard/account_34_view.xml',
         'wizard/book_cash_view.xml',
         'views/menu.xml',
     ],
