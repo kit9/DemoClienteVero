a5a6d498fd930d967b4583b2bdb02f9006fd3267
Cesar Barraza <cbarraza@vallegrande.edu.pe>
2019-07-11 16:12:11 -0500

    Update account_30.py

--- libreria/VG/wizard/account_30.py
+++ libreria/VG/wizard/account_30.py
@@ -6,7 +6,8 @@ import time
 _logger = logging.getLogger(__name__)
 
 
-class Account_17(models.TransientModel):
+    # INICIO 001 "CREACION DE LA CLASE"
+class account_30(models.TransientModel):
     _name = "libreria.account_30"
     _description = "Cuenta_30"
 
@@ -14,54 +15,83 @@ class Account_17(models.TransientModel):
     txt_filename = fields.Char('filename', readonly=True)
     txt_binary = fields.Binary('file', readonly=True)
 
+    # FIN 001
+
     @api.multi
     def generate_file(self):
-        # filtro de fecha
-        dominio = [('dummy_account_id.code', 'like', '30}')]
+
+    # INICIO 005 "MODIFICADO EL MODELO A BUSCAR CON FILTRO"
 
         # modelo a buscar
-        lst_account_move_line = self.env['account.move'].search(dominio)
+        lst_account_move_line = self.env['account.move.line'].search([('account_id.code', 'ilike', '30')])
+
+    # FIN 005
 
         # variables creadas
         content_txt = ""
+        _estado_ope = ""
 
+    # INICIO 002 "AGREGADO DE CAMPOS CON CONDICIONALES"
 
         # Iterador
         for line in lst_account_move_line:
 
+            # debe
+            #if len(line.debit) > 0:
 
+
+            # validador de estado de operación
+            if line.create_date.strftime("%m%Y") == time.strftime("%m%Y"):
+                _estado_ope = "1"
+            else:
+                if line.create_date.strftime("%Y") != time.strftime("%Y"):
+                    _estado_ope = "8"
+                else:
+                    if int(time.strftime("%m")) == int(time.strftime("%m")) - 1:
+                        _estado_ope = "9"
+                    else:
+                        _estado_ope = "1"
+
+    # FIN 002
+
+    # INICIO 003 "AGREGANDO CAMPOS"
 
             # datos a exportar a txt
 
-            txt_line = "%s|%s|M%s|%s|%s|%s|%s|%s|%s|%s|%s|%s" % (
-                line.date.strftime("%Y/%m/%d") or '',  # 1 fecha en formato codigo
-                line.ref or '', #2 nombre de la factura
-                line.x_studio_field_fwlP9 or '', #3 codigo de almacenamiento
-                '', #4 codigo de la compañia a quien se brindo el servicio
-                line.partner_id.vat or '', #5 ruc de la empresa
-                line.partner_id or '', #6 nombre de la empresa
-                '', #7 fecha de elaboración
-                '', #8 total de monto a cobrar
-                 '',
+            txt_line = "%s|%s|M%s|%s|%s|%s|%s|%s|%s|%s" % (
+                line.date.strftime("%Y/%m/%d") or '',  # 1
+                line.name or '',
+                line.id or '',
+                line.partner_id.catalog_06_id.code or '',
+                line.partner_id.vat or '',
+                line.invoice_id.date_document or '',
+                line.partner_id.name or '',
+                line.account_id.code or '',
                 '',
-                '',
-                '',
+                _estado_ope or ''
             )
+
+    # FIN 003
 
             # Agregamos la linea al TXT
             content_txt = content_txt + "" + txt_line + "\r\n"
 
+    # INICIO 004 "CONVIRTIENDO TXT A BINARIO"
+
         self.write({
             'state': 'get',
             'txt_binary': base64.b64encode(content_txt.encode('ISO-8859-1')),
-            'txt_filename': "Ple. Cuenta 30"
+            'txt_filename': "Ple cuenta_30.txt"
         })
+    # FIN 004
+
         return {
             'type': 'ir.actions.act_window',
-            'name': 'Ple. Cuenta 30',
-            'res_model': 'libreria.account_30',
+            'name': 'Ple. cuenta_30',
+            'res_model': 'libreria.account_46',
             'view_mode': 'form',
             'view_type': 'form',
             'res_id': self.id,
             'target': 'new'
         }
+# Fin
