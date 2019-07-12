1425fa6792454a3190d7811fe110341835e545bb
Anthony Loayza <31226781+tonimontana1697@users.noreply.github.com>
2019-07-12 10:29:11 -0500

    Update sales.py

--- libreria/VG/wizard/sales.py
+++ libreria/VG/wizard/sales.py
@@ -65,10 +65,10 @@ class Sales(models.TransientModel):
             # datos a exportar a txt
 
             txt_line = "%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s" % (
-                line.date_document or '',  # 1
+                line.date_document.strftime("%d-%m-%Y") if self.date_invoice else "",  # 1
                 line.number or '',  # 2
                 line.move_id.x_studio_field_fwlP9 or '', # 3
-                line.date_invoice.strftime("%d-%m-%Y") if self.date_invoice else "",  # 4
+                line.date_invoice or '',  # 4
                 line.date_due or '',  # 5
                 line.document_type_id.id or '',  # 6
                 line.invoice_serie or '',  # 7
