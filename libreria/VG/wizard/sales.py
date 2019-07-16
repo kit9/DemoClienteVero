f520b9752a49f3f8c6c6cafc15180505582fd6fe
Jose Luis Condori Jara <jcondori@vallegrande.edu.pe>
2019-07-15 22:52:52 -0500

    Corrección

--- libreria/VG/wizard/sales.py
+++ libreria/VG/wizard/sales.py
@@ -9,6 +9,7 @@
 # --          #005   26/06/2019          ANTHONY LOAYZA        MODIFICADO EL MODELO A BUSCAR CON FILTRO.               #
 # --          #006   26/06/2019          ANTHONY LOAYZA        AGREGADO DE VALIDADOR DE ERROR.                         #
 # --          #007   26/06/2019          ANTHONY LOAYZA        MODIFICADO LA CONDICIONAL DEBIT.                        #
+# --          #008   15/07/2019          JOSE CONDORI          CORRECCION DE ERROR CON FECHA.                          #
 # ######################################################################################################################
 
 from odoo import models, fields, api
@@ -18,7 +19,8 @@ import time
 
 _logger = logging.getLogger(__name__)
 
-    # INICIO 001 "CREACION DE LA CLASE"
+
+# INICIO 001 "CREACION DE LA CLASE"
 class Sales(models.TransientModel):
     _name = "libreria.sales"
     _description = "Sales"
@@ -35,19 +37,19 @@ class Sales(models.TransientModel):
         # modelo a buscar
         lst_account_move_line = self.env['account.invoice'].search([])
 
-    # INICIO 006 "AGREGADO DE VALIDADOR DE ERROR"
+        # INICIO 006 "AGREGADO DE VALIDADOR DE ERROR"
 
         # validador de error
-        #if len(lst_account_move_line) == 0:
+        # if len(lst_account_move_line) == 0:
         #    raise ValidationError("No se encuentra la venta")
 
-    # FIN 006
+        # FIN 006
 
         # variables creadas
         content_txt = ""
         _pay = ""
 
-    # INICIO 002 "AGREGADO DE CAMPOS CON CONDICIONALES"
+        # INICIO 002 "AGREGADO DE CAMPOS CON CONDICIONALES"
 
         # Iterador
         for line in lst_account_move_line:
@@ -58,29 +60,30 @@ class Sales(models.TransientModel):
             else:
                 _pay = ""
 
-
-
-    # INICIO 003 "AGREGANDO CAMPOS"
+            # INICIO 003 "AGREGANDO CAMPOS"
 
             # datos a exportar a txt
 
             txt_line = "%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s" % (
-                line.date_document.strftime("%d-%m-%Y") if self.date_document else "",  # 1
+                # 008 - Incio - Coreccion de error con campo fecha
+                line.date_document.strftime("%d-%m-%Y") if line.date_document else "",  # 1
+                # line.date_document.strftime("%d-%m-%Y") if self.date_document else "",  # 1
+                # 008 - Fin - Coreccion de error con campo fecha
                 line.number or '',  # 2
-                line.move_id.x_studio_field_fwlP9 or '', # 3
+                line.move_id.x_studio_field_fwlP9 or '',  # 3
                 line.date_invoice or '',  # 4
                 line.date_due or '',  # 5
                 line.document_type_id.id or '',  # 6
                 line.invoice_serie or '',  # 7
                 line.invoice_number or '',  # 8
-                '', # 9
+                '',  # 9
                 line.partner_id.catalog_06_id.code or '',  # 10
-                line.partner_id.vat or '', # 11
-                line.partner_id.name or '', # 12
-                '', #13
-                line.amount_untaxed or '', #14
-                '', # 15
-                line.amount_tax or '', #16
+                line.partner_id.vat or '',  # 11
+                line.partner_id.name or '',  # 12
+                '',  # 13
+                line.amount_untaxed or '',  # 14
+                '',  # 15
+                line.amount_tax or '',  # 16
                 '',  # 17
                 '',  # 18
                 '',  # 19
@@ -88,11 +91,11 @@ class Sales(models.TransientModel):
                 '',  # 21
                 '',  # 22
                 '',  # 23
-                line.amount_total or '', # 24
-                line.currency_id.name or '', # 25
-                line.exchange_rate or '', # 26
+                line.amount_total or '',  # 24
+                line.currency_id.name or '',  # 25
+                line.exchange_rate or '',  # 26
                 line.date_invoice or '',  # 27
-                line.document_type_id.id or '', # 28
+                line.document_type_id.id or '',  # 28
                 line.invoice_serie or '',  # 29
                 line.invoice_number or '',  # 30
                 '',  # 31
@@ -102,19 +105,19 @@ class Sales(models.TransientModel):
                 '',  # 35
             )
 
-    # FIN 003
+            # FIN 003
 
             # Agregamos la linea al TXT
             content_txt = content_txt + "" + txt_line + "\r\n"
 
-    # INICIO 004 "CONVIRTIENDO TXT A BINARIO"
+        # INICIO 004 "CONVIRTIENDO TXT A BINARIO"
 
         self.write({
             'state': 'get',
             'txt_binary': base64.b64encode(content_txt.encode('ISO-8859-1')),
             'txt_filename': "venta.txt"
         })
-    # FIN 004
+        # FIN 004
 
         return {
             'type': 'ir.actions.act_window',
