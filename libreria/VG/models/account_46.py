3aa1420fed95ef4d6b31c36afb0bba23269fad22
Anthony Loayza <31226781+tonimontana1697@users.noreply.github.com>
2019-07-16 20:20:36 -0500

    corrigiendo errores de ple cuenta 46

--- libreria/VG/models/account_46.py
+++ libreria/VG/models/account_46.py
@@ -8,11 +8,11 @@ class account_46(models.Model):
     _inherit = 'account.move.line'
 
     # Para filtrar
-    month_year_move = fields.Char(compute="_get_year_move", store=True, copy=False)
+    year_move_line = fields.Char(compute="_get_year_move", store=True, copy=False)
 
     @api.multi
     @api.depends('create_date')
     def _get_month_move(self):
         for rec in self:
             if rec.create_date:
-                rec.year_move = rec.create_date.strftime("%Y")
+                rec.year_move_line = rec.create_date.strftime("%Y")
