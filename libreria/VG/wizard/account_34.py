f1c7dee9f7af8ac8e427aa9694b822e518e4d490
Junior Rejas Yman <jrejas@vallegrande.edu.pe>
2019-08-08 11:27:39 -0500

    Update account_34.py

--- libreria/VG/wizard/account_34.py
+++ libreria/VG/wizard/account_34.py
@@ -35,7 +35,7 @@ class account_34(models.TransientModel):
             txt_line = "%s|%s|M%s|%s|%s|%s|%s|%s|%s|%s|%s|%s" % (
                 line.date.strftime("%Y/%m/%d") or '',
                 line.move_id.name or '',
-                '',
+                line.id or'',
                 '',
                 '',
                 '',
